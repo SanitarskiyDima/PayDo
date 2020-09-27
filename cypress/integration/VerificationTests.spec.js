14350f386df9e6d9c23cfd5ef5715968704e69a4
Dmitriy Sanitarskiy <dmitriy.sanitarskiy@takeaway.com>
2020-09-28 01:34:52 +0300

    update tests

--- cypress/integration/VerificationTests.spec.js
+++ cypress/integration/VerificationTests.spec.js
@@ -12,18 +12,18 @@ import businessVerPage from "../elements/BusinessVerPage";
 
         describe('Login and Personal Verification', () => {
 
-            // it('Are you US person or you do business incorporated in US', () => {
+            it('Are you US person or you do business incorporated in US', () => {
 
-            //     loginPage.getButtonToAdmibPanel().click();
-            //     homePage.getCheckUrl()
-            //     homePage.getMenuVerification().click();
-            //     cy.wait(1000)
-            //     homePage.getSubmenuPersonal().click();
-            //     persVerPage.getCheckUrl();
-            //     persVerPage.getRadioInputUSA().click();
-            //     persVerPage.isElementDisplayed();
-            //     persVerPage.sendMainInformationPersonalDocuments()
-            // })
+                loginPage.getButtonToAdmibPanel().click();
+                homePage.getCheckUrl()
+                homePage.getMenuVerification().click();
+                cy.wait(1000)
+                homePage.getSubmenuPersonal().click();
+                persVerPage.getCheckUrl();
+                persVerPage.getRadioInputUSA().click();
+                persVerPage.isElementDisplayed();
+                //persVerPage.sendMainInformationPersonalDocuments()
+            })
 
             it('Personal Verification', () => {
 
